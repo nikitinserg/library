098f161bfa0de63d388ab22b9496e8a0064c7d3c
nikitinserg <nikitin.serg.alex@gmail.com>
2021-08-25 13:31:34 +1000

    added AuthorList

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
+import AuthorList from "./components/Authors";
 
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = [];
+        this.state = {
+            'authors': []
+        }
     }
 
-    render(){
+    componentDidMount() {
+        const authors = [
+            {
+                'first_name': "Ivan",
+                'last_name': "Ivanov",
+                'birthday_year': 1990
+            },
+            {
+                'first_name': "Petr",
+                'last_name': "Petrov",
+                'birthday_year': 1991
+            }
+        ]
+        this.setState(
+            this.state = {
+                'authors': authors
+            }
+        )
+    }
+
+    render() {
         return (
-            <div>Main App</div>
+            <AuthorList authors={this.state.authors}/>
         )
     }
 }
